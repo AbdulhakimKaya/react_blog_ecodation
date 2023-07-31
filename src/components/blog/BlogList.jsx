66db30b5e5925a0a3290990c00fd56f2c1f1af4f
Abdulhakim KAYA <abdulhakimkaya2199@gmail.com>
2023-07-31 21:55:52 +0300

    BlogList

--- src/components/blog/BlogList.jsx
+++ src/components/blog/BlogList.jsx
@@ -1,13 +1,74 @@
 import React, {Component} from 'react';
+import {withTranslation} from "react-i18next";
+import BlogApi from "../../services/BlogApi";
+
 
 class BlogList extends Component {
+
+    // component'deki isim
+    static displayName = "Blog_List"
+
+    // constructor
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            blogList: [],
+        }
+    }
+
+    // CDM
+    componentDidMount() {
+        BlogApi.blogServiceList()
+            .then((response) => {
+                console.log(response.data)
+                this.setState({
+                    blogList: response.data
+                })
+            })
+            .catch((err) => {
+                console.log('Error: ', err)
+            })
+    }
+
     render() {
+        // object destructing
+        const {t} = this.props
+        const {blogList} = this.state
         return (
-            <div>
-                
-            </div>
+            <React.Fragment>
+                <h1 className={"text-center display-4 text-uppercase"}>Blog List</h1>
+                <table className="table table-hover table-striped">
+                    <thead>
+                    <tr>
+                        <th>{t('blog_id')}</th>
+                        <th>{t('blog_header')}</th>
+                        <th>{t('blog_content')}</th>
+                        <th>{t('date')}</th>
+                        <th>{t('update')}</th>
+                        <th>{t('view')}</th>
+                        <th>{t('delete')}</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        blogList.map((temp) => (
+                            <tr key={temp.id}>
+                                <td>{temp.id}</td>
+                                <td>{temp.header}</td>
+                                <td>{temp.content}</td>
+                                <td>{temp.systemDate}</td>
+                                <td><i className="fa-solid fa-pen-nib text-primary"></i></td>
+                                <td><i className="fa-solid fa-binoculars text-warning"></i></td>
+                                <td><i className={"fa-solid fa-trash text-danger"}></i></td>
+                            </tr>
+                        ))
+                    }
+                    </tbody>
+                </table>
+            </React.Fragment>
         );
     }
 }
 
-export default BlogList;
+export default withTranslation()(BlogList);
